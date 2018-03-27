f4b1265328eb51e06e45a1a69602d6c9e0a446fb
Benjamin <ben.webb@planswift.com>
2018-03-27 16:58:16 -0600

    more stuff

--- src/app/models/Calendar.ts
+++ src/app/models/Calendar.ts
@@ -1,3 +1,19 @@
 export class Calendar{
     timestamp:number;
 }
+
+var year = {
+    count: 2018,
+    child: {
+        month: 12,
+        days: 365.24,
+    },
+    parent: { 
+        millenium: 1e3,
+        age: 1e9,
+        epoch: 1e10,
+        period: 1e11,
+        era: 1e11,
+        eon: 1e15
+    }
+}
